f88e49290c8caefbc2ea542ea290ca34458d2f7c
Arthur Clemens <arthur@visiblearea.com>
2013-09-19 17:22:36 +0200

    Rename action for multi purpose

--- actions/action_bulk_commands_done.erl
+++ actions/action_bulk_commands_done.erl
@@ -1,9 +1,9 @@
--module(action_bulk_commands_delete_done).
+-module(action_bulk_commands_done).
 -include("zotonic.hrl").
 
 -export([render_action/4]).
 
 render_action(_TriggerId, _TargetId, Args, Context) ->
     % Pass all arguments
-    Script = [<<"action_bulk_commands.onDeleteDone(">>, z_utils:js_object(Args, Context), $), $; ],
+    Script = [<<"action_bulk_commands.onDone(">>, z_utils:js_object(Args, Context), $), $; ],
 	{Script, Context}.
